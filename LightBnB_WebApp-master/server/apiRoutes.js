925aaf16b70c1007454243c2c5f93ac4649a86f6
ps-yu <pankajsharma12540@gmail.com>
2023-04-22 13:09:11 -0400

    Fix the api routes for all properties

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -2,6 +2,7 @@ module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
     database.getAllProperties(req.query, 20)
+
     .then(properties => res.send({properties}))
     .catch(e => {
       console.error(e);
